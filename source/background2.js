30dea97b0bd3edeb030731137fabc44254ed2e35
killerbean26 <34723829+killerbean26@users.noreply.github.com>
2019-03-06 23:52:14 +0300

    imgur redirection added

--- source/background2.js
+++ source/background2.js
@@ -2,10 +2,16 @@ chrome.webRequest.onBeforeRequest.addListener(
     function(temp) {
         var active_url = temp.url;
         var host = active_url.match(/(https?:\/\/|www\.?)(www\.)?([^\/\s]+)/ig)[0];
-        host = host.replace("wikipedia.org","0wikipedia.org");
+       if (host.includes("wikipedia.org")){
+            host = host.replace("wikipedia.org","0wikipedia.org");
+        }
+        else if (host.includes("imgur.com")){
+            host = host.replace("imgur.com","0imgur.com");
+        }
         var new_url = document.createElement('a');
         new_url.href = active_url;
         var path = new_url.pathname;
         return {redirectUrl:host+path};
-    },{urls: ["*://wikipedia.org/*","*://www.wikipedia.org/*","*://*.wikipedia.org/*",],
+      
+    },{urls: ["*://wikipedia.org/*","*://www.wikipedia.org/*","*://*.wikipedia.org/*","*://*.imgur.com/*"],
 },["blocking"]);
